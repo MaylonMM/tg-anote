5ef3a871d0255cfd6b8265fcbdf7d9859f8a8e43
Maylon Magri <maylonmagri@gmail.com>
2018-11-03 13:29:33 -0300

    Ajustando cor da statusbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,7 +61,8 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
+      this.statusBar.styleLightContent();
+      this.statusBar.backgroundColorByHexString('0060FF');
       this.splashScreen.hide();
     });
   }
